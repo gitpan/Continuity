dc1674687be573c2e4db2a710a8517a27c8f8583
Brock Wilcox <awwaiid@thelackthereof.org>
2009-06-06 21:36:35 +0000

    Import of AWWAIID/Continuity-1.01 from CPAN.
    gitpan-cpan-distribution: Continuity
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         AWWAIID/Continuity-1.01.tar.gz
    gitpan-cpan-author:       AWWAIID
    gitpan-cpan-maturity:     released

--- lib/Continuity.pm
+++ lib/Continuity.pm
@@ -1,6 +1,6 @@
 package Continuity;
 
-our $VERSION = '1.0';
+our $VERSION = '1.01';
 
 =head1 NAME
 
@@ -475,7 +475,7 @@ L<Continuity::Adapt::HttpDaemon>, L<Coro>
 
 =head1 COPYRIGHT
 
-  Copyright (c) 2004-2008 Brock Wilcox <awwaiid@thelackthereof.org>. All
+  Copyright (c) 2004-2009 Brock Wilcox <awwaiid@thelackthereof.org>. All
   rights reserved.  This program is free software; you can redistribute it
   and/or modify it under the same terms as Perl itself.
 
